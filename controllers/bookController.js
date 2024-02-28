bf1ab8f1ae7418413aac611882b7937292661f04
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2024-02-28 12:45:06 +0530

    code: fixed response bug

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,7 +38,7 @@ exports.book_list = asyncHandler(async (req, res, next) => {
     .populate("author")
     .exec();
 
-  res.render("book_list", { title: "Book List", book_list: allBooks });res.send("NOT IMPLEMENTED: Book list");
+  res.render("book_list", { title: "Book List", book_list: allBooks });
 });
 
 // Display detail page for a specific book.
